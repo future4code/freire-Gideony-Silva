570350cbe65b494d72ed4a349538393476d0c874
Gideony Ewerton <gideonyewerton@gmail.com>
2022-06-09 16:55:19 -0300

    parte de adcionar post foi adicionado

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -24,9 +24,28 @@ class App extends React.Component {
       { nomeUsuario: 'vitor', 
       fotoUsuario: 'https://picsum.photos/50/30', 
       fotoPost: 'https://picsum.photos/200/150' }
-    ] 
+    ]
 
+   
   }
+
+  adicionaPost = () => {
+
+   const valorPost = {
+      nomeUsuario: this.state.nomeUsuario,
+      fotoUsuario: 'https://picsum.photos/50/30',
+      fotoPost: 'https://picsum.photos/200/150'
+    } 
+
+    const copyListPost = [...this.state.post, valorPost]
+      this.setState({post:copyListPost}) 
+
+  };
+
+  onChangeInputNome = (event) => {this.setState({valorPost: {nomeUsuario: event.target.value}});}
+  onChangeInputFoto = (event) => {this.setState({valorPost: {fotoUsuario: event.target.value}});}
+  onChangeInputPost = (event) => {this.setState({valorPost: {fotoPost: event.target.value}});}
+
 
   render() {
     const listComponentPost = this.state.post.map((obj) => {
@@ -41,6 +60,13 @@ class App extends React.Component {
     return (
       <MainContainer>
 
+      <input
+            
+            onChange={this.onChangeInputNome}
+            placeholder={"Nome"}
+          />
+
+        <button onClick={this.adicionaPost}>Adicionar</button>
       <div>{listComponentPost}</div>
 
       </MainContainer>
